a0ad416fc4ea32ba2a3d826f0571fb68198668fe
Medik2007 <meeedik2007@gmail.com>
2023-10-17 19:22:45 +0300

    configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,5 @@
 DIR="$HOME/.config/polybar"
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -q top -c "$DIR"/config.ini &
+polybar -q top-1 -c "$DIR"/config.ini &
+polybar -q top-2 -c "$DIR"/config.ini &
