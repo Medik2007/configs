11a3eaa75e63b162d5b4a2f91a6a0f6e71110af9
Medik2007 <meeedik2007@gmail.com>
2023-10-26 00:59:39 +0300

    Configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,9 +43,11 @@ require("lazy").setup({
 
 require("nvim-tree").setup()
 require('lualine').setup()
-require'nvim-treesitter.configs'.setup {ensure_installed = { "c", "lua", "python", "javascript", "html", "css", },  highlight = {enable = true}}
-require('nvim-ts-autotag').setup()
+require'nvim-treesitter.configs'.setup {ensure_installed = { "c", "lua", "python", "javascript", "html", "css", "htmldjango"},  highlight = {enable = true}}
 require('nvim-autopairs').setup()
+require('nvim-ts-autotag').setup({
+  filetypes = { "html" , "xml" , "htmldjango" },
+})
 
 vim.cmd[[colorscheme tokyonight-night]]
 
